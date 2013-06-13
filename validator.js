8577c096bc3d21961b65f337a452d7532287fd44
Samuel Gabel <samuel.gabel@project-a.com>
2013-06-13 17:57:14 +0200

    Prepare for modularity

--- validator.js
+++ validator.js
@@ -1,8 +1,6 @@
 var request = require('request'),
 	j2h = require('json2html'),
 	fs = require('fs'),
-	url = require('url'),
-	check = require('validator').check,
 	w3url = 'http://validator.w3.org/check',
 	uriOpt = '?uri=',
 	fragmentOpt = '?fragment=',
@@ -11,76 +9,14 @@ var request = require('request'),
 	changedFolder = 'changed/',
 	diffsFound = 0,
 	errors = {},
-	buildNo = false,
 	remember = false,
-	urls = [];
+	buildNo = false;
 
 var NEW_ERROR = "new",
 	OLD_ERROR = "gone";
 
-if( process.argv.length < 3 ) {
-	help();
-	process.exit(1);
-}
-
-function help() {
-	console.log("Usage: " + process.argv[0] + ' ' + process.argv[1].substring( process.argv[1].lastIndexOf('/')+1,process.argv[1].length));
-	console.log("\t--urls URLS\t[mandatory]\tSpace-separated list of urls to test");
-	console.log("\t--remember \t[optional]\tWhether we should remember the current status and compare with last, or just get the results");
-	console.log("\t--build X\t[optional]\tNumber of current build - used to save the current status");
-	console.log("\t--help\t\tDisplay this message");
-}
-
-process.argv.forEach( function(val, index, array){
-	if(val.indexOf('--') === 0) {
-		var option = val.substring(2);
-		switch(option) {
-			case 'urls':
-				for(var i = index+1; i<array.length && array[i].indexOf('--') === -1 ; i++){
-					try {
-						check(array[i]).isUrl();
-						urls.push(array[i]);
-					} catch(ValidatorError) {
-						console.log("Please provide valid URLS.\n'" + array[i] + "' doesn't count as one.");
-						help();
-						process.exit(1);
-					}
-				}
-				break;
-			case 'remember':
-				remember = true;
-				break;
-			case 'build':
-				if( array.length > index+1 ) {
-					var buildNoArray = array[index+1].match(/^\d+$/);
-					if( buildNoArray.length === 1 ) {
-						buildNo = buildNoArray[0];
-						break;
-					}
-				}
-				console.log("Option --build takes a single number as argument");
-				help();
-				process.exit(1);
-				break;
-			case 'help':
-				help();
-				process.exit(0);
-				break;
-			default:
-				console.log("Unrecognized option: " + val);
-				help();
-				process.exit(1);
-		}
-	}
-});
-
-if( urls.length ) {
-	processUrls();
-} else {
-	console.log( "Please provide a list of URLs" );
-	help();
-	process.exit(1);
-}
+var validator = {};
+module.exports = validator;
 
 process.on('exit', function() {
 	console.log( "Done." );
@@ -97,14 +33,16 @@ process.on('exit', function() {
 	}
 });
 
-function processUrls( ) {
-	urls.forEach( function( url, index ) {
+validator.processUrls = function( options ) {
+	remember = options.remember;
+	buildNo = options.buildNo;
+	options.urls.forEach( function( url, index ) {
 		setTimeout( function() {
 			console.log( "Checking " + url );
 			errors[ url ] = {
 				nu: false,
-			ol: false,
-			changes: []
+				ol: false,
+				changes: []
 			};
 			if(remember) {
 				readOldErrors( url );
@@ -112,9 +50,9 @@ function processUrls( ) {
 			getW3sOpinion( url );
 		}, 1000*index);
 	});
-}
+};
 
-function readOldErrors( url ) {
+validator.internal.readOldErrors = function readOldErrors( url ) {
 	var fileName = getJsonFileName( url );
 	fs.exists( fileName, function( exists ) {
 		if( !exists ) {
@@ -143,9 +81,33 @@ function readOldErrors( url ) {
 		}
 		compareAndSaveIfDone();
 	});
-}
+};
 
-function getW3sOpinion( url ) {
+validator.internal.requestCallback = function( error, resp, body ) {
+	if( !error ) {
+		if( resp && resp.statusCode === 200 ) {
+			try {
+				var j = JSON.parse( body );
+				console.log( "Setting new data for " + url );
+				errors[ url ].nu = j;
+			} catch( e ) {
+				console.log( "Error in parsing new data " + e );
+				console.log( "Data: " + body );
+				errors[ url ].nu = {};
+			}
+		} else {
+			console.log("Error in request: " + JSON.stringify(resp, null, 4));
+		}
+	} else {
+		console.log( "Error in request for " + url + ": " + error );
+		errors[ url ].nu = {};
+	}
+	if(remember) {
+		compareAndSaveIfDone();
+	}
+};
+
+validator.internal.getW3sOpinion = function getW3sOpinion( url ) {
 	console.log("requesting for " + url);
 	var requestOptions = {
 		'url': w3url + uriOpt + url + outputOpt,
@@ -153,32 +115,10 @@ function getW3sOpinion( url ) {
 			'user-agent': "Mozilla/5.0"
 		}
 	};
-	request(requestOptions, function( error, resp, body ) {
-		if( !error ) {
-			if( resp && resp.statusCode === 200 ) {
-				try {
-					var j = JSON.parse( body );
-					console.log( "Setting new data for " + url );
-					errors[ url ].nu = j;
-				} catch( e ) {
-					console.log( "Error in parsing new data " + e );
-					console.log( "Data: " + body );
-					errors[ url ].nu = {};
-				}
-			} else {
-				console.log("Error in request: " + JSON.stringify(resp, null, 4));
-			}
-		} else {
-			console.log( "Error in request for " + url + ": " + error );
-			errors[ url ].nu = {};
-		}
-		if(remember) {
-			compareAndSaveIfDone();
-		}
-	});
-}
+	request(requestOptions, requestCallback);
+};
 
-function compareAndSaveIfDone() {
+validator.internal.compareAndSaveIfDone = function compareAndSaveIfDone() {
 	if( areWeDone() ) {
 		Object.keys( errors ).forEach( function( url ) {
 			console.log( "Checking for data of " + url );
@@ -191,9 +131,9 @@ function compareAndSaveIfDone() {
 			}
 		});
 	}
-}
+};
 
-function findChanges( nu, ol, url ) {
+validator.internal.findChanges = function findChanges( nu, ol, url ) {
 	var changesFound = false;
 	console.log( "Comparing " );
 	if( !nu || !ol ) {
@@ -251,35 +191,35 @@ function findChanges( nu, ol, url ) {
 		}
 	}
 	return changesFound;
-}
+};
 
-function isNotJustInfo( msg ) {
+validator.helpers.isNotJustInfo = function isNotJustInfo( msg ) {
 	return ( msg.type !== 'info' || ( msg.subtype && msg.subtype === 'warning' ) );
-}
+};
 
-function areWeDone() {
+validator.helpers.areWeDone = function areWeDone() {
 	var notDone = Object.keys( errors ).some( function( e ) {
 		if( errors[e].nu === false || errors[e].ol === false ) {
 			return true;
 		}
 	});
 	return !notDone;
-}
+};
 
-function getJsonFileName( urlName ) {
+validator.helpers.getJsonFileName = function getJsonFileName( urlName ) {
 	return saveFolder + urlName.replace( /\//g, '-' ) + '.json';
-}
+};
 
-function getHtmlFileName( urlName ) {
+validator.helpers.getHtmlFileName = function getHtmlFileName( urlName ) {
 	return saveFolder + urlName.replace( /\//g, '-' ) + '.html';
-}
+};
 
-function getHtmlChangeFileName( urlName ) {
+validator.helpers.getHtmlChangeFileName = function getHtmlChangeFileName( urlName ) {
 	var b = buildNo ? '.' + buildNo : '';
 	return changedFolder + urlName.replace( /\//g, '-' ) + b + '.html';
-}
+};
 
-function saveFile( url ) {
+validator.helpers.saveFile = function saveFile( url ) {
 	try {
 		var data = JSON.stringify( errors[url].nu, null, 4 );
 		var html = j2h.render( errors[url].nu );
@@ -302,9 +242,9 @@ function saveFile( url ) {
 	} catch( e ) {
 		console.log( "Error in JSON" );
 	}
-}
+};
 
-function printChanges() {
+validator.helpers.printChanges = function printChanges() {
 	Object.keys( errors ).forEach( function( e ) {
 		if( errors[e].changes.length ) {
 			errors[e].changes.forEach( function( c ) {
@@ -312,8 +252,8 @@ function printChanges() {
 			});
 		}
 	});
-}
+};
 
-function chopSlashAtEnd( str ) {
+validator.helpers.chopSlashAtEnd = function chopSlashAtEnd( str ) {
 	return ( str.lastIndexOf( '/' ) === str.length -1 ) ? str.slice(0, -1) : str;
-}
+};
